6e7e805614fde9619d86cf9f830126291c9d3833
Student <example@example.com>
2019-08-07 10:46:13 -0700

    continue through lesson

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,5 +1,7 @@
 class Album < ApplicationRecord
   has_many :songs, dependent: :destroy
+  has_many :album_artists
+  has_many :artists, :through => :album_artists
   validates :name, presence: true
   validates_length_of :name, maximum: 100
   before_save(:titleize_album)
